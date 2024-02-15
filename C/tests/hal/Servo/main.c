cf278daa5b8a4761a451f9ff28d01a9042ce5834
MohamedRefat13 <mo7amedrefaat2001@gmail.com>
2024-02-16 00:40:01 +0200

    Adding Servo Driver

---
+++ C/tests/hal/Servo/main.c
@@ -0,0 +1,24 @@
+#include "common/Types.h"
+#include "common/Utils.h"
+#include "common/Config.h"
+#include "common/Registes.h"
+#include "hal/Servo/Servo.h"
+#include <util/delay.h>
+int main()
+{
+	Servo_Init(SERVO_OC1B_PIN);
+
+	while (1)
+	{
+		for (int i = 0; i <= 180 ; i+=10)
+		{
+			Servo_SetAngle(i, SERVO_OC1B_PIN);
+			_delay_ms(500);
+		}
+		for (int i = 180; i >= 0 ; i-=10)
+		{
+			Servo_SetAngle(i, SERVO_OC1B_PIN);
+			_delay_ms(500);
+		}
+	}
+}
