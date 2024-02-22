d1990f0b802b6beb9c5c41986bc384c0433e3eee
nouragamall <noura36mohamed@gmail.com>
2024-02-22 22:56:16 +0200

    GPIO  enhancement

--- C/mcal/GPIO/GPIO.c
+++ C/mcal/GPIO/GPIO.c
@@ -1,7 +1,7 @@
-#include "../../common/Types.h"
-#include "../../common/Utils.h"
-#include "../../common/Config.h"
-#include "../../common/Registes.h"
+#include "Config.h"
+#include "Types.h"
+#include "Utils.h"
+#include "Registes.h"
 #include "GPIOPrivate.h"
 #include "GPIO.h"
 
@@ -21,7 +21,7 @@ error_t GPIO_SetPinDirection(port_t portNumber, pin_t pinNumber,
 				case kPORTB : SET_DIR_OUTPUT(GPIOB_DIR_REG, pinNumber); break;
 				case kPORTC : SET_DIR_OUTPUT(GPIOC_DIR_REG, pinNumber); break;
 				case kPORTD : SET_DIR_OUTPUT(GPIOD_DIR_REG, pinNumber); break;
-				#if MCU_TYPE == _PIC
+				#if IS_PIC()
 				case kPORTE : SET_DIR_OUTPUT(GPIOE_DIR_REG, pinNumber); break;
 				#endif
 			}
@@ -34,12 +34,17 @@ error_t GPIO_SetPinDirection(port_t portNumber, pin_t pinNumber,
 				case kPORTB : SET_DIR_INPUT(GPIOB_DIR_REG, pinNumber); break;
 				case kPORTC : SET_DIR_INPUT(GPIOC_DIR_REG, pinNumber); break;
 				case kPORTD : SET_DIR_INPUT(GPIOD_DIR_REG, pinNumber); break;
-				#if MCU_TYPE == _PIC
+				#if IS_PIC()
 				case kPORTE : SET_DIR_INPUT(GPIOE_DIR_REG, pinNumber); break;
 				#endif
 			}
 		}
-	}else
+		else
+		{
+			retErrorState = kFunctionParameterError;
+		}
+	}
+	else
 	{
 		retErrorState = kFunctionParameterError;
 	}
@@ -62,7 +67,7 @@ error_t GPIO_SetPinValue(port_t portNumber, pin_t pinNumber,
 				case kPORTB : SET_BIT(GPIOB_OUT_REG, pinNumber); break;
 				case kPORTC : SET_BIT(GPIOC_OUT_REG, pinNumber); break;
 				case kPORTD : SET_BIT(GPIOD_OUT_REG, pinNumber); break;
-				#if MCU_TYPE == _PIC
+				#if IS_PIC()
 				case kPORTE : SET_BIT(GPIOE_OUT_REG, pinNumber); break;
 				#endif
 			}
@@ -75,10 +80,14 @@ error_t GPIO_SetPinValue(port_t portNumber, pin_t pinNumber,
 				case kPORTB : CLR_BIT(GPIOB_OUT_REG, pinNumber); break;
 				case kPORTC : CLR_BIT(GPIOC_OUT_REG, pinNumber); break;
 				case kPORTD : CLR_BIT(GPIOD_OUT_REG, pinNumber); break;
-				#if MCU_TYPE == _PIC
+				#if IS_PIC()
 				case kPORTE : CLR_BIT(GPIOE_OUT_REG, pinNumber); break;
 				#endif
 			}
+		}
+		else
+		{
+			retErrorState = kFunctionParameterError;
 		}
 	}else
 	{
@@ -98,7 +107,7 @@ error_t GPIO_GetPinValue(port_t portNumber, pin_t pinNumber, state_t * state)
       case kPORTB : *state= GET_BIT(GPIOB_IN_REG, pinNumber); break;
       case kPORTC : *state= GET_BIT(GPIOC_IN_REG, pinNumber); break;
       case kPORTD : *state =GET_BIT(GPIOD_IN_REG, pinNumber); break;
-	  #if MCU_TYPE == _PIC
+	  #if IS_PIC()
 	  case kPORTE : *state =GET_BIT(GPIOE_IN_REG, pinNumber); break;
 	  #endif
     }
@@ -114,73 +123,51 @@ error_t GPIO_GetPinValue(port_t portNumber, pin_t pinNumber, state_t * state)
 error_t GPIO_SetPortDirection(port_t portNumber, uint8_t direction)
 {
  error_t retErrorState = kNoError;
-  /* Make sure that the Port number is in the valid range */
-  if (portNumber <= GPIO_LAST_REG  )
-  {
-
     switch (portNumber)
     {
      case kPORTA : GPIOA_DIR_REG = direction; break;
      case kPORTB : GPIOB_DIR_REG = direction; break;
      case kPORTC : GPIOC_DIR_REG = direction; break;
      case kPORTD : GPIOD_DIR_REG = direction; break;
-	 #if MCU_TYPE == _PIC
+	 #if IS_PIC()
 	 case kPORTE : GPIOE_DIR_REG = direction; break;
 	 #endif
+	 default:retErrorState = kFunctionParameterError;break;
     }
-  }
-  else
-  {
-   retErrorState = kFunctionParameterError;
-  }
   return retErrorState;
 }
 
 error_t GPIO_SetPortValue(port_t portNumber, uint8_t pintVoltageLevel)
 {
- /* Make sure that the Port number is in the valid range */
  error_t retErrorState = kNoError;
- if (portNumber <= GPIO_LAST_REG  )
- {
    switch (portNumber)
    {
     case kPORTA : GPIOA_OUT_REG = pintVoltageLevel; break;
     case kPORTB : GPIOB_OUT_REG = pintVoltageLevel; break;
     case kPORTC : GPIOC_OUT_REG = pintVoltageLevel; break;
     case kPORTD : GPIOD_OUT_REG = pintVoltageLevel; break;
-	#if MCU_TYPE == _PIC
+	#if IS_PIC()
 	case kPORTE : GPIOE_OUT_REG = pintVoltageLevel; break;
 	#endif
+	default: retErrorState=kFunctionParameterError;break;
    }
- }
- else
- {
-  retErrorState = kFunctionParameterError;
- }
  return retErrorState;
 }
 
 error_t GPIO_GetPortValue(port_t portNumber, uint8_t * state)
 {
- /* Make sure that the Port number is in the valid range */
   error_t retErrorState = kNoError;
-  if (portNumber <= GPIO_LAST_REG  )
-  {
     switch (portNumber)
     {
      case kPORTA : *state = GPIOA_IN_REG; break;
      case kPORTB : *state = GPIOB_IN_REG; break;
      case kPORTC : *state = GPIOC_IN_REG; break;
      case kPORTD : *state = GPIOD_IN_REG; break;
-	 #if MCU_TYPE == _PIC
+	 #if IS_PIC()
 	 case kPORTE : *state = GPIOE_IN_REG; break;
 	 #endif
+	 default:retErrorState = kFunctionParameterError;break;
     }
-  }
-  else
-  {
-   retErrorState = kFunctionParameterError;
-  }
   return retErrorState;
 }
 
@@ -203,7 +190,7 @@ error_t GPIO_SetPinPullup(port_t portNumber, pin_t pinNumber)
 			case kPORTD : SET_DIR_INPUT(GPIOD_DIR_REG, pinNumber);
 						  SET_BIT(GPIOD_OUT_REG, pinNumber);
 						  break;
-			#if MCU_TYPE == _PIC
+			#if IS_PIC()
 			case kPORTE : SET_DIR_INPUT(GPIOE_DIR_REG, pinNumber);
 						  SET_BIT(GPIOE_OUT_REG, kHigh);
 						  break;
