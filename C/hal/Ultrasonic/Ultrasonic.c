04d0efd9cc741b5ffd3e6da9e2c493acbf5b581b
noran97 <noranmahmoud597@gmail.com>
2024-02-23 10:02:58 +0200

    distance calc

--- C/hal/Ultrasonic/Ultrasonic.c
+++ C/hal/Ultrasonic/Ultrasonic.c
@@ -10,7 +10,7 @@
 #include <util/delay.h>
 #endif
 static uint16 t1 =0, t2=0;
-static uint8_t flag = 0;
+static uint8_t flag = 0, Capflag=0;
 //-----------------------------------------------------------------------------
 //                           HELPER FUNCTIONS
 //-----------------------------------------------------------------------------
@@ -18,7 +18,7 @@ static void Ultrasonic_Trigger(Ultrasonic_t * pUltrasonicConfig)
 {
     GPIO_SetPinValue(pUltrasonicConfig->port, pUltrasonicConfig->triggerPin, kHigh);//IGNORE-STYLE-CHECK[L004]
     #if IS_AVR()
-    _delay_us(1000);
+    _delay_us(10);
     #endif
     GPIO_SetPinValue(pUltrasonicConfig->port, pUltrasonicConfig->triggerPin, kLow);//IGNORE-STYLE-CHECK[L004]
 }
@@ -28,13 +28,13 @@ void Ultrasonic_ISR()
     {
         ICU_ReadCaptureValue(&t1);
         flag = 1;
-       // GPIO_SetPinValue(kPORTD,kPIN0,0);
         ICU_SelectEdge(ICU_FALLING_EDGE);
     }
     else if (flag == 1)
     {
         ICU_ReadCaptureValue(&t2);
-        ICU_Disable();
+        Capflag=1;
+       // ICU_Disable();
         flag =0;
     }
 }
@@ -50,8 +50,9 @@ error_t Ultrasonic_Init(Ultrasonic_t * pUSConfig)
     {
         GPIO_SetPinDirection(pUSConfig ->port, pUSConfig ->triggerPin, kOutput);
         GPIO_SetPinDirection(pUSConfig ->port, pUSConfig ->echoPin, kInput);
-         GIC_voidGlobalInterruptControl(GLOBAL_INTERRUPT_ENABLE);
         ICU_SelectEdge(ICU_RISING_EDGE);
+        ICU_SetCallBackFunc(Ultrasonic_ISR);
+        GIC_voidGlobalInterruptControl(GLOBAL_INTERRUPT_ENABLE);
     }
     else
     {
@@ -61,15 +62,17 @@ error_t Ultrasonic_Init(Ultrasonic_t * pUSConfig)
 }
 float  Ultrasonic_GetDistance(Ultrasonic_t * pUSConfig)
 {
-    float distance = 0;
+    ICU_Init(ICU_PRESCALER_1);
     Ultrasonic_Trigger(pUSConfig);
-    ICU_SetCallBackFunc(Ultrasonic_ISR);
-     ICU_Init(ICU_PRESCALER_64);
-      #if IS_AVR()
-    _delay_us(1000);
-    #endif
-    distance = (t2 - t1)*4/125;
-    distance = (distance /2)* 343/1000;
+    float distance = 0;
+    if (Capflag==1)
+    {
+    uint16 ticks=t2-t1;
+    distance = (ticks*(1.0/8000000));
+    distance = distance*34300/2;
+    Capflag=0;
+    ICU_SelectEdge(ICU_RISING_EDGE);
+    }
    // *distance = (uint16)((pulseWidth*100)/58);
     return distance;
 }
